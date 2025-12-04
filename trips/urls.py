499bb8b443ea532d484a895d05ec1868c56c0f9a
achraf-hh <badreddineachraf03@gmail.com>
2025-12-04 20:59:52 +0100

    Add remaining files

--- trips/urls.py
+++ trips/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import PlanView, ConfirmTripView, ConfirmedTripsListView
 
+from .views import ConfirmedTripsListView, ConfirmTripView, PlanView
 
 urlpatterns = [
     path('plan/', PlanView.as_view(), name='generate-plan'),
